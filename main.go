55e4de3e8f82b20010a283db4522699b7431e048
jebari ahmed kacem <ahmedkacem.jebari@esprit.tn>
2025-11-13 02:06:10 +0100

    fetching location + pagination

--- main.go
+++ main.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
-
-	
 )
 
 
 func main(){
 	initCliCommands()
 	exit := false
+	scanner := bufio.NewScanner(os.Stdin)
+	
 	for !exit {
 		fmt.Print("Pokedex >")
 		
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
+		
+		if !scanner.Scan(){
+			break
+		}
+
+
 		err := scanner.Err()
 		if err != nil{
 			log.Fatal(err)
@@ -34,6 +41,25 @@ func main(){
 					
 					if err.Error() == "exit"{
 						os.Exit(0)
+					}else if err.Error() == "map" {
+						mapCommand := supportedCommands["map"]
+						locations, err := getLocations(&mapCommand)
+						if err != nil{
+							break
+						}
+						for _, v := range locations.Results {
+							fmt.Printf("%s \n",v.Name)
+						}
+					}else if err.Error() == "bmap" {
+						bmapCommand := supportedCommands["bmap"]
+						locations, err := getLocations(&bmapCommand)
+						if err != nil{
+							fmt.Printf("%v\n",err)
+							break
+						}
+						for _, v := range locations.Results {
+							fmt.Printf("%s \n",v.Name)
+						}
 					}
 				}
 			}
@@ -49,24 +75,89 @@ func commandExit()error{
 	return fmt.Errorf("exit")
 }
 func commandHelp()error{
-
-	fmt.Printf("Welcome to the Pokedex! \nUsage:\n\n")
+	fmt.Printf("\nWelcome to the Pokedex! \nUsage:\n\n")
 	for _,v := range supportedCommands{
 		fmt.Printf("%s: %s\n",v.name,v.description)
 	}
 	return fmt.Errorf("help")
 }
+func commandMap()error{
+	return fmt.Errorf("map")
+}
+func commandBMap()error{
+	return fmt.Errorf("bmap")
+}
+
+func getLocations(mapCommand *cliCommand)(location,error){
+	url := ""
+	
+	switch mapCommand.name {
+		case "map" :
+			url = mapCommand.conf.Next
+		case "bmap" :
+			if mapCommand.conf.Previous == ""{return location{},fmt.Errorf("you're on the first page")}
+			url = mapCommand.conf.Previous
+	}
+
+	res, err := http.Get(url)
+	if err != nil{
+		return location{}, err
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return location{}, err
+	}
+	
+	var locations location
+	if err := json.Unmarshal(data, &locations); err != nil {
+		return location{},err
+	}
+	mapCommand.conf.Next = locations.Next
+	mapCommand.conf.Previous = locations.Previous
+	return locations, nil
+	
+
+}
+type location struct{
+	Count 	int `json:"count"`
+	Next 	string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct{
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	} `json:"results"`
+}
+
 
 type cliCommand struct{
 	name 		string
 	description string
 	callback 	func() error
+	conf		*config
 }
+
+type config struct{
+	Next 		string
+	Previous 	string
+}
+
 
 var supportedCommands = make(map[string]cliCommand)
 
 
 func initCliCommands(){
-	supportedCommands["exit"] = cliCommand{name:"exit",description:"Exit the Pokedex",callback:commandExit}
+
+	initConfig := &config{
+		Next :"https://pokeapi.co/api/v2/location-area/?limit=20&offset=0",
+		Previous: "",
+	}
+
 	supportedCommands["help"] = cliCommand{name:"help",description:"Display A Help Message",callback:commandHelp}
+	supportedCommands["exit"] = cliCommand{name:"exit",description:"Exit the Pokedex",callback:commandExit}
+	supportedCommands["map"] = cliCommand{name:"map",description:"Display the next 20 locations",callback:commandMap,conf:initConfig}
+	supportedCommands["bmap"] = cliCommand{name:"bmap",description:"Display the previous 20 locations",callback:commandBMap,conf:initConfig}
+
 }
+
